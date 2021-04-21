382b374f400468accf7b53ee633d775d85e0c791
Maxibax <priymak615@gmail.com>
2021-04-21 14:22:13 +0300

    Update: add data list

--- src/script/pages/List.js
+++ src/script/pages/List.js
@@ -1,4 +1,8 @@
+// Class
 import Component from "../class/Component"
+// Data
+import games from "../data/games"
+// Help
 
 export default class List extends Component {
     constructor(root, parent) {
@@ -13,21 +17,28 @@ export default class List extends Component {
         this.btn = undefined
     }
 
+    template() {
+        return `
+            <div class="container">
+                <div class="list">
+                    <ul class="list__sked">
+                        ${games.map(item => `
+                          <li data-card data-link="game${item.link}" class="list__sked-item">
+                              <img src="${item.image}" alt="${item.link}">          
+                          </li>`).join('\n')}
+                    </ul>
+                </div>
+            </div>`
+    }
+
     initConstructor() {
         this.btn = document.body.querySelector('button')
     }
 
     init() {
-        this.btn.addEventListener('click', () => {
-            this.link('/game2048')
-        })
-    }
-
-    template() {
-        return `
-            <h1>TEST</h1>
-            <button>Toch me</button>
-        `
+        // this.btn.addEventListener('click', () => {
+        //     this.link('/game2048')
+        // })
     }
 
     link(url) {
